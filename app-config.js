4cad63ca39584bf965442dd9fe3c40db5454dc39
Gabriel-estagiariocode <estagiario.ti@quata.sp.gov.br>
2026-05-20 09:08:03 -0300

    Correção Icone e Nome instalador

--- app-config.js
+++ app-config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   supabaseUrl: 'https://vqrogebjodwaioyjcrws.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxcm9nZWJqb2R3YWlveWpjcndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NDMxNjMsImV4cCI6MjA5MTQxOTE2M30.m9VnXhQebf-U0Ivn3nkO-wSM9dweYMjLNs8tYvXB0fI',
   recaptchaSiteKey: '6LfBFtIsAAAAAAO3pz90sd3q8rcuUsxDxgcD2ciS',
-  desktopInstallerUrl: '/downloads/SiteEstoque-Setup-1.0.0.exe'
+  desktopInstallerUrl: '/downloads/VeloStock-Setup-1.0.0.exe'
 };
